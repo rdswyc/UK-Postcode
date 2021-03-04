7800f9fe15679a78f1c9d28863302da26b7de078
Rodrigo Chin <rodrigo.chin@outlook.com>
2021-03-03 22:02:41 -0300

    Check for invalid postcodes

--- UK-Postcode/wwwroot/app.js
+++ UK-Postcode/wwwroot/app.js
@@ -4,11 +4,16 @@
   angular.module('app', [])
     .controller('Controller', function ($http) {
       var self = this;
+      self.error = false;
       self.list = [];
 
       self.getAddress = function () {
         $http.get('address/' + self.postCode).then(function (response) {
           addAddress(response.data);
+          self.error = false;
+          self.postCode = '';
+        }, function (response) {
+          self.error = true;
         });
       };
 
@@ -17,7 +22,6 @@
         address.distanceMI = Math.round(address.distanceMI);
         address.distanceKM = Math.round(address.distanceKM);
         self.list.push(address);
-        self.postCode = '';
       };
     });
 })();
